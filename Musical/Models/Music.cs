d4ff36fc82ef3aee5ab2b08906cf944dc3801021
Mariam Alaeddin <mariamalaeddin@gmail.com>
2020-03-12 09:40:55 -0700

    add indeces an d controllers, create routes and input min html

--- Musical/Models/Music.cs
+++ Musical/Models/Music.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 
 namespace Organzier.Models
 {
-  public class Sounds
+  public class Sound
   {
     public string Artist { get; set; }
     public string Song { get; set; }
     public string Genre { get; set; }
+    private static List<Sound> _instances = new List<Sound> {};
     
+    public Sounds(string artist, string song, string genre)
+    {
+      Artist = artist;
+      Song = song;
+      Genre = genre; 
+    }
+    
+
   }
 }
